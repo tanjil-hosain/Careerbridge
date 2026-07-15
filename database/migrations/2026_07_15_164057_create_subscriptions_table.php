745d832e88510a5f2a1ec9e7cd4f1f624f431a0b
tanjil-hosain <tanjilh85@gmail.com>
2026-07-15 22:44:23 +0600

    subscription mcr

---
+++ database/migrations/2026_07_15_164057_create_subscriptions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('plan_id')->constrained()->cascadeOnDelete();
+            $table->integer('amount');
+            $table->integer('remaining_jobs');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('payment_status', [
+                'pending',
+                'paid',
+                'failed'
+            ])->default('pending');
+
+            $table->enum('status', [
+                'active',
+                'expired'
+            ])->default('active');
+
+            $table->string('transaction_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
