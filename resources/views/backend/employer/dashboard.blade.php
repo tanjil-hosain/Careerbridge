e893cd965bd0805eb271cd770d18cbb9e2915abe
tanjil-hosain <tanjilh85@gmail.com>
2026-07-22 20:08:41 +0600

    plan

--- resources/views/backend/employer/dashboard.blade.php
+++ resources/views/backend/employer/dashboard.blade.php
@@ -52,6 +52,66 @@
             </div>
         </div>
 
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="card shadow-sm">
+                    <div class="card-header">
+                        Subscription Details
+                    </div>
+                    <div class="card-body">
+                        @if ($subscription)
+                            <p>
+                                <b>Plan :</b>
+                                {{ $subscription->plan->name }}
+                            </p>
+                            <p>
+                                <b>Status :</b>
+                                <span class="badge bg-success">
+                                    {{ ucfirst($subscription->status) }}
+                                </span>
+                            </p>
+                            <p>
+                                <b>Remaining :</b>
+                                {{ $subscription->remaining_limit }}
+                            </p>
+                            <p>
+                                <b>Expire :</b>
+
+                                {{ $subscription->end_date }}
+                            </p>
+                            <a href="{{ route('employer.subscription.plans') }}" class="btn btn-warning">
+                                Upgrade Plan
+                            </a>
+                        @else
+                            <div class="alert alert-warning">
+                                No Subscription Found
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="card shadow-sm">
+                    <div class="card-header">
+                        Quick Actions
+                    </div>
+                    <div class="card-body d-grid gap-2">
+                        <a href="{{ route('employer.jobs.create') }}" class="btn btn-light">
+                            Post Job
+                        </a>
+                        <a href="{{ route('employer.jobs.index') }}" class="btn btn-light">
+                            Manage Jobs
+                        </a>
+                        <a href="{{ route('employer.subscription.my') }}" class="btn btn-light">
+                            My Subscription
+                        </a>
+                        <a href="{{ route('employer.subscription.plans') }}" class="btn btn-info">
+                            Upgrade Plan
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
