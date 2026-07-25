e2790e98740dd467c0ab71557de28bb7c8e917c1
tanjil-hosain <tanjilh85@gmail.com>
2026-07-26 00:30:05 +0600

    modify

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -64,42 +64,30 @@ export default function Home({ jobs, categories, companies }) {
               </div>
             </div>
           </section>
-          <div className="section-box wow animate__animated animate__fadeIn mt-70">
-            <div className="container">
-              <div className="box-swiper">
-                <div className="swiper-container swiper-group-6">
-                  <div className="swiper-wrapper pb-70 pt-5">
-                    <div className="swiper-slide hover-up">
-                      <div className="item-logo"><a href="candidates-single.html"><img alt="jobhub" src="/frontend_assets/imgs/slider/logo/google.svg" /></a>
-                      </div>
-                    </div>
-                    <div className="swiper-slide hover-up">
-                      <div className="item-logo"><a href="candidates-single.html"><img alt="jobhub" src="/frontend_assets/imgs/slider/logo/airbnb.svg" /></a>
-                      </div>
-                    </div>
-                    <div className="swiper-slide hover-up">
-                      <div className="item-logo"><a href="candidates-single.html"><img alt="jobhub" src="/frontend_assets/imgs/slider/logo/dropbox.svg" /></a>
-                      </div>
-                    </div>
-                    <div className="swiper-slide hover-up">
-                      <div className="item-logo"><a href="candidates-single.html"><img alt="jobhub" src="/frontend_assets/imgs/slider/logo/fedex.svg" /></a>
-                      </div>
-                    </div>
-                    <div className="swiper-slide hover-up">
-                      <div className="item-logo"><a href="candidates-single.html"><img alt="jobhub" src="/frontend_assets/imgs/slider/logo/wallmart.svg" /></a>
-                      </div>
-                    </div>
-                    <div className="swiper-slide hover-up">
-                      <div className="item-logo"><a href="candidates-single.html"><img alt="jobhub" src="/frontend_assets/imgs/slider/logo/hubspot.svg" /></a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-button-next" />
-                <div className="swiper-button-prev" />
+<div className="section-box wow animate__animated animate__fadeIn mt-70">
+  <div className="container">
+    <div className="box-swiper">
+      <div className="swiper-container swiper-group-6">
+        <div className="swiper-wrapper pb-70 pt-5">
+          {companies && companies.map((company, index) => (
+            <div className="swiper-slide hover-up" key={company.id || index}>
+              <div className="item-logo">
+                <Link href={`/jobs?company=${company.id}`}>
+                  <img 
+                    alt={company.company_name || "jobhub"} 
+                    src={company.logo ? `/storage/${company.logo}` : `/frontend_assets/imgs/slider/logo/brand-${index + 1}.svg`} 
+                  />
+                </Link>
               </div>
             </div>
-          </div>
+          ))}
+        </div>
+      </div>
+      <div className="swiper-button-next" />
+      <div className="swiper-button-prev" />
+    </div>
+  </div>
+</div>
           <section className="section-box">
             <div className="container">
               <div className="row align-items-end">
