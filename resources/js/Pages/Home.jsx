6bd6136f1dd08c50f0f2a1466e5dbe5a5f27ea13
tanjil-hosain <tanjilh85@gmail.com>
2026-07-26 23:52:49 +0600

    modify

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link, useForm } from '@inertiajs/react';
 import FrontendLayout from '@/Layouts/FrontendLayout';
 import Banner from '@/Components/Banner';
+import CompanyLogo from '@/Components/CompanyLogo';
 
-export default function Home({ jobs, categories, companies }) {
+export default function Home({ jobs, categories }) {
   // const { data, setData, post, processing, reset } = useForm({
   //   email: '',
   // });
@@ -20,30 +21,8 @@ export default function Home({ jobs, categories, companies }) {
       <FrontendLayout>
         <main className="main">
           <Banner/>
-<div className="section-box wow animate__animated animate__fadeIn mt-70">
-  <div className="container">
-    <div className="box-swiper">
-      <div className="swiper-container swiper-group-6">
-        <div className="swiper-wrapper pb-70 pt-5">
-          {companies && companies.map((company, index) => (
-            <div className="swiper-slide hover-up" key={company.id || index}>
-              <div className="item-logo">
-                <Link href={`/jobs?company=${company.id}`}>
-                  <img 
-                    alt={company.company_name || "jobhub"} 
-                    src={company.logo ? `/storage/${company.logo}` : `/frontend_assets/imgs/slider/logo/brand-${index + 1}.svg`} 
-                  />
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="swiper-button-next" />
-      <div className="swiper-button-prev" />
-    </div>
-  </div>
-</div>
+          <CompanyLogo/>
+
           <section className="section-box">
             <div className="container">
               <div className="row align-items-end">
